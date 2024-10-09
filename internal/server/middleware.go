94cbaf893ebbcd164a25252f1584cb3816fd2797
Bon Nicolai Mercado <bonhokage06@gmail.com>
2024-10-09 16:59:32 +0800

    fix: remove wallet address limiting

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -1,18 +1,14 @@
 package server
 
 import (
-	"errors"
 	"fmt"
-	"net"
 	"net/http"
-	"strings"
 	"sync"
 	"time"
 
 	"github.com/jellydator/ttlcache/v2"
 	"github.com/kataras/hcaptcha"
 	log "github.com/sirupsen/logrus"
-	"github.com/urfave/negroni"
 )
 
 type Limiter struct {
@@ -33,42 +29,26 @@ func NewLimiter(proxyCount int, ttl time.Duration) *Limiter {
 }
 
 func (l *Limiter) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	address, err := readAddress(r)
-	if err != nil {
-		var mr *malformedRequest
-		if errors.As(err, &mr) {
-			renderJSON(w, claimResponse{Message: mr.message}, mr.status)
-		} else {
-			renderJSON(w, claimResponse{Message: http.StatusText(http.StatusInternalServerError)}, http.StatusInternalServerError)
-		}
-		return
-	}
-
 	if l.ttl <= 0 {
 		next.ServeHTTP(w, r)
 		return
 	}
 
-	clintIP := getClientIPFromRequest(l.proxyCount, r)
+	clintIP := getClientIPFromRequest(w, r)
+	if clintIP == "" {
+		return
+	}
 	l.mutex.Lock()
-	if l.limitByKey(w, address) || l.limitByKey(w, clintIP) {
+	if l.limitByKey(w, clintIP) {
 		l.mutex.Unlock()
 		return
 	}
-	l.cache.SetWithTTL(address, true, l.ttl)
 	l.cache.SetWithTTL(clintIP, true, l.ttl)
 	l.mutex.Unlock()
-
-	next.ServeHTTP(w, r)
-	if w.(negroni.ResponseWriter).Status() != http.StatusOK {
-		l.cache.Remove(address)
-		l.cache.Remove(clintIP)
-		return
-	}
 	log.WithFields(log.Fields{
-		"address":  address,
 		"clientIP": clintIP,
 	}).Info("Maximum request limit has been reached")
+	next.ServeHTTP(w, r)
 }
 
 func (l *Limiter) limitByKey(w http.ResponseWriter, key string) bool {
@@ -80,28 +60,14 @@ func (l *Limiter) limitByKey(w http.ResponseWriter, key string) bool {
 	return false
 }
 
-func getClientIPFromRequest(proxyCount int, r *http.Request) string {
-	if proxyCount > 0 {
-		xForwardedFor := r.Header.Get("X-Forwarded-For")
-		if xForwardedFor != "" {
-			xForwardedForParts := strings.Split(xForwardedFor, ",")
-			// Avoid reading the user's forged request header by configuring the count of reverse proxies
-			partIndex := len(xForwardedForParts) - proxyCount
-			if partIndex < 0 {
-				partIndex = 0
-			}
-			return strings.TrimSpace(xForwardedForParts[partIndex])
-		}
-	}
-	//get the client IP address from the request
+func getClientIPFromRequest(w http.ResponseWriter, r *http.Request) string {
 	remoteIP := r.Header.Get("X-Real-IP")
 	if remoteIP != "" {
 		return remoteIP
 	}
-
-	remoteIP, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		remoteIP = r.RemoteAddr
+	if remoteIP == "" {
+		errMsg := "The client address is missing"
+		renderJSON(w, claimResponse{Message: errMsg}, http.StatusInternalServerError)
 	}
 	return remoteIP
 }
